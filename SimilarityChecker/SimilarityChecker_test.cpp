e497f70a901323ba40e3d478eaf6b81a29541c5c
thelogic0 <thelogic@gmail.com>
2026-07-01 15:31:01 +0900

    [feature] full and nothing case

--- SimilarityChecker/SimilarityChecker_test.cpp
+++ SimilarityChecker/SimilarityChecker_test.cpp
@@ -1,7 +1,33 @@
 #include "gmock/gmock.h"
 #include "SimilarityChecker.cpp"
 
-TEST(SimilarityChecker, TEST1)
+using namespace testing;
+
+class SimilarityCheckerFixture : public Test {
+public:
+	SimilarityChecker checker;
+};
+
+TEST_F(SimilarityCheckerFixture, LengthCheckOnly1)
 {
-	EXPECT_EQ(1, 1);
+	double actual = checker.getScore("AAA", "BBB");
+	EXPECT_EQ(SimilarityChecker::MAX_LENGTH_SCORE, actual);
+}
+
+TEST_F(SimilarityCheckerFixture, LengthCheckOnly2)
+{
+	double actual = checker.getScore("AAAAAAA", "BCDEFGH");
+	EXPECT_EQ(SimilarityChecker::MAX_LENGTH_SCORE, actual);
+}
+
+TEST_F(SimilarityCheckerFixture, LengthCheckDoubleCase)
+{
+	double actual = checker.getScore("AAA", "BCDEFG");
+	EXPECT_EQ(0, actual);
+}
+
+TEST_F(SimilarityCheckerFixture, LengthCheckDoubleCase2)
+{
+	double actual = checker.getScore("AAA", "BCDEFGHIJ");
+	EXPECT_EQ(0, actual);
 }
